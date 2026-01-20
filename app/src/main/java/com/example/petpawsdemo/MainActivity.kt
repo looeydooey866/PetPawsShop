03c87036c4bb29ed50356de981f79a0d74316231
looeydooey866 <looeydooey866@gmail.com>
2026-01-20 10:48:57 +0800

    .

--- app/src/main/java/com/example/petpawsdemo/MainActivity.kt
+++ app/src/main/java/com/example/petpawsdemo/MainActivity.kt
@@ -6,18 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,10 +57,18 @@ class MainActivity : ComponentActivity() {
                         "Dog Cage to appease your dogs"
                     )
                     Column(modifier = Modifier.padding(innerPadding).fillMaxWidth(1.0f).fillMaxHeight(1.0f).verticalScroll(scrollState)) {
-                        Text(
-                            text = "Dog Items",
-                            fontSize = 40.sp,
-                        )
+                        Column(
+                            modifier = Modifier.fillMaxWidth(1.0f).padding(20.dp),
+                            verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically)
+                        ) {
+                            Text(
+                                text = "Dog Items",
+                                fontSize = 35.sp,
+                            )
+                            Row(
+                                modifier = Modifier.fillMaxWidth(1.0f).height(3.dp).background(Color.Black),
+                            ){}
+                        }
                         val mod = Modifier
                             .aspectRatio(6f / 7)
                             .background(Color.White)
